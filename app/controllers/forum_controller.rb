f13c7a860fddc8bc651c4cdce70d61ba38cceaba
Kyle-Williamson-Dev <kwilliamson0232@gmail.com>
2022-02-09 18:37:43 -0700

    added forums controller and auth for it

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -1,4 +1,13 @@
 class ForumController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+
+    if user_signed_in?
+      redirect_to new_user_session_path
+    end   
+    
   end
+
 end
+
